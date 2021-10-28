bb5ba8827f233773b2f9f4a1593d61f6fdcb18b9
devversion <paulgschwendtner@gmail.com>
2021-10-28 14:48:28 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { MatFormFieldModule } from '@angular/material-experimental/mdc-form-field';
-import { MatInputModule } from '@angular/material-experimental/mdc-input';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    BrowserModule
-  ],
+  imports: [BrowserAnimationsModule, BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
